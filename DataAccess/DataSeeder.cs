4d665d61b9efa1d529758ccfc82e119e421fd6a8
jakob <jakob@JAKES_DATA>
2024-11-29 18:37:34 +0100

    Add project files.

---
+++ DataAccess/DataSeeder.cs
@@ -0,0 +1,23 @@
+﻿using Core.Models;
+using System.Linq;
+
+namespace DataAccess
+{
+    public static class DataSeeder
+    {
+        public static void SeedData(AppDbContext context)
+        {
+            if (!context.TempHumidityRecords.Any())
+            {
+                context.TempHumidityRecords.AddRange(new[]
+                {
+                    new TempHumidityRecord { Date = DateTime.Now.AddDays(-6), Temperature = 12.5, Humidity = 65, IsIndoor = true },
+                    new TempHumidityRecord { Date = DateTime.Now.AddDays(-5), Temperature = 8.0, Humidity = 70, IsIndoor = false },
+                    new TempHumidityRecord { Date = DateTime.Now.AddDays(-4), Temperature = 10.0, Humidity = 75, IsIndoor = true }
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Sql Lab 3/CsvImporter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Core.Models;
+using Core.Services;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+public static class CsvImporter
+{
+    /// <summary>
+    /// Importerar data från en CSV-fil och beräknar mögelrisk för varje post.
+    /// </summary>
+    /// <param name="filePath">Sökvägen till CSV-filen.</param>
+    /// <returns>Lista med poster och deras mögelrisk.</returns>
+    public static List<(TempHumidityRecord Record, double MoldRisk)> Import(string filePath)
+    {
+        var records = new List<TempHumidityRecord>();
+
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+            BadDataFound = null,
+            MissingFieldFound = null,
+            HeaderValidated = null
+        };
+
+        try
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, config))
+            {
+                records = csv.GetRecords<TempHumidityRecord>().ToList();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ett fel inträffade vid inläsning av CSV-filen: {ex.Message}");
+            throw;
+        }
+
+        // Beräkna mögelrisk för varje post
+        var recordsWithMoldRisk = records.Select(r => (Record: r, MoldRisk: CalculationService.CalculateMoldRisk(r))).ToList();
+        return recordsWithMoldRisk;
+    }
+}
